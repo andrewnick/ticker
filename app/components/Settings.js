ea73f1d56b1d76bc55791f74e059f2e339dcab33
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2019-08-10 08:12:50 +1200

    adding dropdown to settings

--- app/components/Settings.js
+++ app/components/Settings.js
@@ -2,24 +2,40 @@ import React, { forwardRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
 import Slide from '@material-ui/core/Slide';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import { Formik } from 'formik';
+import Select from '@material-ui/core/Select';
 
 const useStyles = makeStyles(theme => ({
   appBar: {
-    position: 'relative'
+    position: 'relative',
+    height: 'auto'
   },
   title: {
     marginLeft: theme.spacing(2),
     flex: 1
+  },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    height: 'auto'
+  },
+  form: {
+    padding: theme.spacing(2)
+  },
+  formControl: {
+    width: '100%'
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
   }
 }));
 
@@ -31,43 +47,69 @@ const Settings = ({ open, handleClose }) => {
   const classes = useStyles();
 
   return (
-    <Dialog
-      fullScreen
-      open={open}
-      onClose={handleClose}
-      TransitionComponent={Transition}
-    >
-      <AppBar className={classes.appBar}>
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            onClick={handleClose}
-            aria-label="close"
-          >
-            <CloseIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Settings
-          </Typography>
-          <Button color="inherit" onClick={handleClose}>
-            save
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <List>
-        <ListItem button>
-          <ListItemText primary="Phone ringtone" secondary="Titania" />
-        </ListItem>
-        <Divider />
-        <ListItem button>
-          <ListItemText
-            primary="Default notification ringtone"
-            secondary="Tethys"
-          />
-        </ListItem>
-      </List>
-    </Dialog>
+    <div>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Settings
+            </Typography>
+            <Button color="inherit" onClick={handleClose}>
+              save
+            </Button>
+          </Toolbar>
+        </AppBar>
+        {/* <Grid>
+          <Grid item> */}
+        <Formik
+          initialValues={{ email: '', name: '' }}
+          onSubmit={(values, { setSubmitting }) => {}}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting
+            /* and other goodies */
+          }) => (
+            <form className={classes.form} onSubmit={handleSubmit}>
+              <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="age-simple">Client</InputLabel>
+                <Select
+                  value={values.age}
+                  onChange={handleChange}
+                  inputProps={{
+                    name: 'age',
+                    id: 'age-simple'
+                  }}
+                >
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </form>
+          )}
+        </Formik>
+        {/* </Grid> */}
+      </Dialog>
+    </div>
   );
 };
 
--- app/components/TopNav.js
+++ app/components/TopNav.js
@@ -1,40 +1,14 @@
 // @flow
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
-import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import Grid from '@material-ui/core/Grid';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import Divider from '@material-ui/core/Divider';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Icon from '@material-ui/core/Icon';
-import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import RestoreIcon from '@material-ui/icons/Restore';
 import SettingsIcon from '@material-ui/icons/Settings';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import EntriesList from './EntriesList';
 import Settings from './Settings';
-import routes from '../constants/routes';
-import styles from './Home.css';
 
 const useStyles = makeStyles(theme => ({
   root: {
